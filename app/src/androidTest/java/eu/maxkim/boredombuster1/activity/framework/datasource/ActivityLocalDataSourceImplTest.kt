ca5e74ee7b4ecb9dd2587897c7175efcb1f88450
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2024-11-15 09:12:36 -0800

    b14: Room database, Test #3

--- app/src/androidTest/java/eu/maxkim/boredombuster1/activity/framework/datasource/ActivityLocalDataSourceImplTest.kt
+++ app/src/androidTest/java/eu/maxkim/boredombuster1/activity/framework/datasource/ActivityLocalDataSourceImplTest.kt
@@ -1,21 +1,44 @@
 package eu.maxkim.boredombuster1.activity.framework.datasource
 
 import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import eu.maxkim.boredombuster1.activity.framework.db.ActivityDao
+import eu.maxkim.boredombuster1.activity.model.Activity
 import eu.maxkim.boredombuster1.framework.AppDatabase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 import java.io.IOException
 
 // since we need a Context to create a database, we will be creating our new test class in the
 // 'androidTest' folder instead of the 'test' one
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class ActivityLocalDataSourceImplTest {
     private lateinit var activityDao: ActivityDao
     private lateinit var database: AppDatabase
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private val activityListObserver: Observer<List<Activity>> = mock()
+
+    @Captor
+    private lateinit var activityListCaptor: ArgumentCaptor<List<Activity>>
 
     // We won't be reusing this logic outside this test class, so there is no need to create a
     // separate Rule
@@ -67,4 +90,24 @@ class ActivityLocalDataSourceImplTest {
         assert(!activityLocalDataSource.isActivitySaved(androidActivity1.key))
     }
 
+    // test the LiveData coming straight from the database
+    @Test
+    fun canSaveActivityToTheDbAndObserveTheLiveData() = runTest {
+        // Arrange
+        val activityLocalDataSource = ActivityLocalDataSourceImpl(activityDao)
+        val expectedList = listOf(androidActivity1, androidActivity2)
+
+        // Act
+        // start observing the database when there are no entries
+        activityLocalDataSource.getActivityListLiveData().observeForever(activityListObserver)
+        activityLocalDataSource.saveActivity(androidActivity1)
+        activityLocalDataSource.saveActivity(androidActivity2)
+
+        // Assert
+        // our first onChanged event will fire with an empty list. We expect our LiveData to be
+        // updated after every save, hence onChanged should be triggered three times.
+        verify(activityListObserver, times(3)).onChanged(activityListCaptor.capture())
+        // the final list should contain both activities.
+        assertEquals(activityListCaptor.value, expectedList)
+    }
 }
