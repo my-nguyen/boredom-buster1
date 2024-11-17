887219e78df97ab248532a0c326e31d1e6e83249
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2024-11-16 19:32:11 -0800

    b21: Test nodes with content description

--- app/src/androidTest/java/eu/maxkim/boredombuster1/activity/ui/newactivity/NewActivityScreenTest.kt
+++ app/src/androidTest/java/eu/maxkim/boredombuster1/activity/ui/newactivity/NewActivityScreenTest.kt
@@ -1,13 +1,21 @@
 package eu.maxkim.boredombuster1.activity.ui.newactivity
 
+import android.content.Context
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.core.app.ApplicationProvider
+import eu.maxkim.boredombuster1.R
 import eu.maxkim.boredombuster1.activity.framework.datasource.androidActivity1
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 
 class NewActivityScreenTest {
 
@@ -36,5 +44,37 @@ class NewActivityScreenTest {
         // method. After that we will call assertIsDisplayed() on it.
         composeTestRule.onNodeWithText(androidActivity1.name)
             .assertIsDisplayed()
+    }
+
+    // test that the Favorite button triggers the onFavoriteClick function
+    @Test
+    fun onFavoriteClickCallbackIsTriggered() {
+        // for interaction with components, especially lambdas, the easiest route is to use Mockito
+        val onFavoriteClick: (isFavorite: Boolean) -> Unit = mock()
+        val isFavorite = false
+
+        composeTestRule.setContent {
+            NewActivityCard(
+                modifier = Modifier.fillMaxWidth(),
+                activity = androidActivity1,
+                isFavorite = isFavorite,
+                onFavoriteClick = onFavoriteClick,
+                onLinkClick = { }
+            )
+        }
+
+        // The content descriptions are stored in the strings.xml file, so we will need a Context to
+        // access them
+        val contentDescription = ApplicationProvider.getApplicationContext<Context>()
+            .getString(R.string.cd_save_activity)
+
+        // not all UI components will have text on them, so we need other means to access their
+        // SemanticsNodeInteraction. the favorite button doesn't have any text, but it does have a
+        // content description. There is another way how we can access a node - with ComposeTestRule
+        // onNodeWithContentDescription() method.
+        composeTestRule.onNodeWithContentDescription(contentDescription)
+            .performClick()
+
+        verify(onFavoriteClick, times(1)).invoke(!isFavorite)
     }
 }
